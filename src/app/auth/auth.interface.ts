c93051a968f557c567e93af99cf79dbd7fb16e40
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-10 17:43:42 +0200

    Add auth module

---
+++ src/app/auth/auth.interface.ts
@@ -0,0 +1,10 @@
+export interface IUserData {
+  email: string,
+  username: string,
+  user_id: number,
+  exp: number
+};
+
+export interface ILoginData {
+  token: string
+};
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { IUserData, ILoginData } from './auth.interface';
+
+import 'jwt-decode/build/jwt-decode'
+
+
+@Injectable()
+export class AuthService implements CanActivate {
+  private _token:string = null;
+  private _currentUser: IUserData = null;
+
+  constructor(private _router: Router) {
+    this._currentUser = JSON.parse(localStorage.getItem('user-data'));
+  }
+
+  canActivate(a: ActivatedRouteSnapshot, s:RouterStateSnapshot): boolean {
+    var result: boolean = !!this._currentUser;
+    if (!result) {
+      this._router.navigate(['login']);
+    }
+
+    return result;
+  }
+
+  setCurrentUser(data:ILoginData):void {
+    this._token = data.token;
+    var userData = jwt_decode(this._token)
+    this._currentUser = userData;
+    localStorage.setItem('user-data', JSON.stringify(userData));
+  }
+
+  clearCurrentUser = () => {
+    this._token = null;
+    this._currentUser = null;
+    localStorage.removeItem('user-data');
+    this._router.navigate(['login']);
+
+  }
+
+  isLogged = ():boolean => !!this._currentUser;
+}
