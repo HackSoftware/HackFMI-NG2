e50266e1bc272241d8723eea968b0c751cd2eef4
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-10 17:44:55 +0200

    Add basin navigation, footer & home modules

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor(private _authService:AuthService) { }
+
+  ngOnInit() {
+  }
+
+  logout = this._authService.clearCurrentUser;
+  isLogged = this._authService.isLogged;
+}
