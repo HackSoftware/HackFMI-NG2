511db3764cd3ba3b1fea741452b94aaa58d91ddb
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-10 17:46:09 +0200

    Add routing in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './auth/auth.service';
+import { HomeComponent } from './home/home.component';
+import { appRoutes } from './app.routing';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavigationComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    appRoutes,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
