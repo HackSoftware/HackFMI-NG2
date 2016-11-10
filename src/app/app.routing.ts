648270719f617698152958c55afa32df53ee8c92
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-10 17:45:28 +0200

    Add app routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthService } from './auth/auth.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthService]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  }
+];
+
+export const appRoutes: ModuleWithProviders = RouterModule.forRoot(routes);
