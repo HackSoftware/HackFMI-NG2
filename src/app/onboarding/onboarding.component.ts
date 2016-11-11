658a78b74b1b5b37e0d2529d6d11511d9799709f
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-11 22:09:07 +0200

    Finish onboarding form

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -15,18 +15,37 @@ export class OnboardingComponent implements OnInit {
   skills: Skill[];
   shirtSizes = ['S', 'M', 'L', 'XL'];
 
+  onboardingInfo = {
+    is_vegetarian: false,
+    needs_work: false,
+    shirt_size: this.shirtSizes[0],
+    social_links: '',
+    known_skills: []
+  }
+
   constructor(private _onboardingService: OnboardingService) {
     this._onboardingService.getSkills()
       .subscribe((data:any) => {
         this.skills = data;
-        console.log(data)
       }, (err) => {
         console.log(err);
       })
   }
 
   onBoardCompetitor(): void {
-    // do stuff
+    // TODO: Send request for onboarding! Authenticate yourself!
+    console.log(this.onboardingInfo);
+  }
+
+  addOrRemoveSkill(id:number): void {
+    var index = this.onboardingInfo.known_skills.indexOf(id);
+    if (index > -1) {
+      this.onboardingInfo.known_skills.splice(index, 1);
+    } else this.onboardingInfo.known_skills.push(id);
+  }
+
+  setShirtSize(size:string):void {
+    this.onboardingInfo.shirt_size = size;
   }
 
   ngOnInit() {
