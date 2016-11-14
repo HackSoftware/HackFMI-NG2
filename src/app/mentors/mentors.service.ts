6021327f9544c690a2dd3621996a94a8db429163
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-14 20:20:17 +0200

    Fix getMentorsListInfo observable return type

--- src/app/mentors/mentors.service.ts
+++ src/app/mentors/mentors.service.ts
@@ -2,15 +2,18 @@ import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
+import { Mentor } from './mentors.models';
 import { AuthHttp } from '../auth/authHttp.service';
 import { ApiUrlsService } from '../core/apiUrls.service';
+
+import 'rxjs/add/operator/map'
 
 
 @Injectable()
 export class MentorsService {
   constructor(private _http: Http, private _apiUrlsService: ApiUrlsService) { }
 
-  getMentorsListInfo():Observable<any> {
+  getMentorsListInfo():Observable<Mentor[]> {
     return this._http.get(this._apiUrlsService.membersPublicListUrl)
                           .map(res => res.json())
   }
