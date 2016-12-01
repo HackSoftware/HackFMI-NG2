33b3a3ca1242b0c93831250c659b26e2324b3b89
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-12-01 16:22:46 +0200

    Remove unnecessary service providing
    * The invites module already provides it

--- src/app/competitors/competitors.module.ts
+++ src/app/competitors/competitors.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { InvitesService } from '../invites/invites.service';
 import { CompetitorsService } from './competitors.service';
 import { CompetitorsResolver } from './competitors.resolver';
 import { CompetitorsComponent } from './competitors.component';
@@ -18,6 +17,6 @@ import { CompetitorsRoutingModule } from './competitors.routing';
     CompetitorsRoutingModule
   ],
   exports: [CompetitorsRoutingModule],
-  providers: [CompetitorsResolver, CompetitorsService, InvitesService],
+  providers: [CompetitorsResolver, CompetitorsService],
 })
 export class CompetitorsModule { }
