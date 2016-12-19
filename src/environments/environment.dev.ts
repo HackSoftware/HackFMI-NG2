4864d9ab1af27123e4a61802f83fac44b4978721
martin95056 <martinangelov056@gmail.com>
2016-12-19 16:48:12 +0200

    Add development environment file

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  protocol: 'https',
+  domain: 'staging.hackbulgaria.com/',
+  apiBaseUrl: 'hackfmi/api/',
+  ws: 'wss',
+  defaultUrlQueryParams: {
+    'format': 'json'
+  }
+};
